89a17e9054fcdd8ffc0e53b7bf3b50fe2173bc0e
Rajan Walia <rajan.walia@paytm.com>
2025-07-01 00:10:31 +0530

    Added Logger, MySQL and Exception

---
+++ src/main/java/com/twitter_backend_spring_boot/twitter/TwitterApplication.java
@@ -0,0 +1,13 @@
+package com.twitter_backend_spring_boot.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter_backend_spring_boot/twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter_backend_spring_boot.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
